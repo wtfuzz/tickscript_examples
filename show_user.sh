10477357b140381c052c2cbc4e4ea64e2b02be66
Matt Thompson <mthompson@hexwave.com>
2016-01-19 17:36:31 -0600

    fixed getting top srcips

--- show_user.sh
+++ show_user.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-curl -s http://localhost:9092/api/v1/user/test |json_reformat
+#curl -s http://localhost:9092/api/v1/user/action_count |json_reformat
+curl -s http://localhost:9092/api/v1/user/top_srcip |json_reformat
